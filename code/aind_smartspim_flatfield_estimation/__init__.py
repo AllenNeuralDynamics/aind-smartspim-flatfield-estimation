ee9109d6a6fa5e692abb6502035a985edad34f64
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2026-09-24 11:33:43 -0700

    bumping pipeline version

--- code/aind_smartspim_flatfield_estimation/__init__.py
+++ code/aind_smartspim_flatfield_estimation/__init__.py
@@ -16,5 +16,5 @@ __maintainers__ = ["Camilo Laiton"]
 __maintainer_emails__ = ["camilo.laiton@alleninstitute.org"]
 __title__ = "aind-smartspim-flatfield-estimation"
 __status__ = "Production"  # 'Production', 'Beta'
-__pipeline_version__ = "5.0.0"
+__pipeline_version__ = "6.0.0"
 __pipeline_name__ = "SmartSPIM Pipeline"
